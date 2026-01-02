0110edd6a226a0393253f78ef4c56493ffdef921
Zahra-hassani <hassanizahra897@gmail.com>
2026-01-02 23:58:13 +0430

    image slider completed with foreign resources

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,31 @@
 import type { NextConfig } from "next";
+import { hostname } from "os";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: '/**'
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: '/**'
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+        port: "",
+        pathname: '/**'
+      },
+    ]
+  }
 };
 
 export default nextConfig;
--- src/app/slider/page.tsx
+++ src/app/slider/page.tsx
@@ -1,27 +1,10 @@
-import Image from 'next/image';
+import ImageSlider from '@/components/ImageSlider';
 import React from 'react'
-import Slider from "react-slick";
 
 function page() {
-     const settings = {
-    dots: true,
-  };
   return (
-    <div className="image-slider-container">
-      <Slider {...settings}>
-        <div>
-          <Image alt='something went wrong' height={400} width={400} src="http://placekitten.com/g/400/200" />
-        </div>
-        <div>
-          <Image alt='something went wrong' height={400} width={400} src="http://placekitten.com/g/400/200" />
-        </div>
-        <div>
-          <Image alt='something went wrong' height={400} width={400} src="http://placekitten.com/g/400/200" />
-        </div>
-        <div>
-          <Image alt='something went wrong' height={400} width={400} src="http://placekitten.com/g/400/200" />
-        </div>
-      </Slider>
+    <div>
+      <ImageSlider />
     </div>
   )
 }
