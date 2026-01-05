8db3e8c0362e00cefc4df1efac6c55f074d96d8b
Zahra-hassani <hassanizahra897@gmail.com>
2026-01-06 00:19:10 +0430

    fetching data by client component from api

---
+++ src/app/image/page.tsx
@@ -0,0 +1,49 @@
+'use client';
+import { error } from 'console';
+import Image from 'next/image';
+import React, { use, useEffect, useState } from 'react'
+
+type Image = {
+    name: string;
+    email: string;
+    phone: string;
+    website: string
+}
+
+ function page() {
+    const [list , setList] = useState<Image[]>([]);
+    const [loading , setLoading] = useState(true);
+    useEffect(()=>{
+        async function fetchData(){
+            try{
+            const response = await fetch("https://jsonplaceholder.typicode.com/users");
+            if(!response.ok) throw new Error('Something went wrong');
+            const data = await response.json();
+            setList(data);
+            }
+            catch(err){
+                console.log("Something go wrong");
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+        fetchData();
+    },[])
+  return loading? (
+    <h1 className='text-2xl font-bold text-center'>Loading...</h1>
+  ):(
+    <div className="grid grid-cols-3 p-5 gap-3">
+        {list.map((user , index)=> (
+            <div key={index} className="flex flex-col items-center rounded gap-3 p-4 border shadow">
+                <h1 className='text-4xl text-center font-bold'>{user.name}</h1>
+                <h2 className="text-xl">{user.email}</h2>
+                <p>{user.phone}</p>
+                <p>{user.website}</p>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default page
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={` antialiased`}
       >
-        <nav className="flex justify-between items-center gap-4 px-3 h-20 w-full bg-white/20 font-bold sticky top-0 left-0">
+        <nav className="flex justify-between backdrop-blur-sm items-center gap-4 px-3 h-20 w-full bg-white/20 font-bold sticky top-0 left-0">
         <Image src="/graduates.png" height={800} width={800} className="h-full w-20" alt="" />
         <ul className="flex justify-between items-center gap-3">
           <li><Link href="/">Home</Link></li>
